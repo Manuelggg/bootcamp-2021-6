c4dc95cdf0d9ecc1ef3f89923675249d36d48a7b
TSOFT\Manuel.Guzman <manuel.guzman@tsoftglobal.com>
2022-01-11 13:04:40 -0300

    Archivos de faregtsoft

---
+++ test-output-thread/report.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "ec8724b4-d885-4089-a41b-b3105eb5b900",
+    "feature": "Google Search",
+    "scenario": "Standard Search One",
+    "start": 1641826683628,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1641826690341,
+    "className": "passed"
+  },
+  {
+    "id": "2c2e74ec-0b8f-4318-804d-191cf5707fef",
+    "feature": "Google Search",
+    "scenario": "Standard Search Two",
+    "start": 1641826690361,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1641826693196,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[TestNG-test\u003dSurefire test-1,5,main]"
+  }
+]);
+});
